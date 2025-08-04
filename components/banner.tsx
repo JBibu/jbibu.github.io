9dfafaa6046cee2adf170253c38c8ada20415446
JBibu <javims@jbibu.xyz>
2025-08-04 17:30:10 +0200

    Changed banner.tsx message

--- components/banner.tsx
+++ components/banner.tsx
@@ -2,7 +2,7 @@
 export function Banner() {
   return (
     <div className="mt-20 w-full bg-yellow-100 text-yellow-800 text-center py-3 px-4 text-sm font-medium shadow-md z-40">
-      🚧 This website is under construction. Some sections may (and will) be incomplete.
+      🚧 This website is under construction. Some sections may be incomplete.
     </div>
   );
 }
